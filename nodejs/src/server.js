212e765478a7b36bbb2c90067161d268dd7a45aa
hip06 <khongpikten21@gmail.com>
2022-06-26 16:15:07 +0700

    8

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -8,8 +8,8 @@ dotenv.config()
 
 const app = express()
 app.use(cors({ origin: process.env.URL_REACT_APP }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '5mb' }))
+app.use(express.urlencoded({ extended: true, limit: '5mb' }))
 
 initWebRoutes(app)
 isConnectedDatabase()
